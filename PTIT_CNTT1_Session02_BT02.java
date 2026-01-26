d6217e1581af2be6fc1046a12be92a21682da80d
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2026-01-26 20:55:59 +0700

    bai tap ss2

---
+++ PTIT_CNTT1_Session02_BT02.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PTIT_CNTT1_Session02_BT02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap ma the loai sach (A, B, C, D): ");
+        char code = sc.next().charAt(0);
+
+        switch (code) {
+            case 'A':
+                System.out.println("Tang 1: Sach Van hoc");
+                break;
+            case 'B':
+                System.out.println("Tang 2: Sach Khoa hoc");
+                break;
+            case 'C':
+                System.out.println("Tang 3: Sach Ngoai ngu");
+                break;
+            case 'D':
+                System.out.println("Tang 4: Sach Tin hoc");
+                break;
+            default:
+                System.out.println("Ma the loai khong hop le!");
+        }
+
+        sc.close();
+    }
+}
+PTIT_CNTT1_Session02_BT01
